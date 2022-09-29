c67ed93f252b16eef14ebf7ca5ca43eeaa0c3383
Tato López <stefanolopez_1992@hotmail.com>
2022-09-29 20:45:16 -0300

    limpieza de anotaciónes y algortimo

--- comisionador.js
+++ comisionador.js
@@ -1,27 +1,3 @@
-/* CONSTRUCTOR */
-
-class MetodoDeCobro{
-    constructor(metodo, comision, diasdecobro, nombre){
-        this.metodo = metodo;
-        this.comision = comision;
-        this.diasdecobro = diasdecobro;
-        this.nombre = nombre;
-    }
-}
-
-/* OBJETOS */
-
-const mpLink0 = new MetodoDeCobro ("Crédito y Débido", 0.0599, 0, "Mercado Pago 0 días" )
-const mpLink10 = new MetodoDeCobro ("Crédito y Débido", 0.0399, 10, "Mercado Pago 10 días" )
-const mpLink18 = new MetodoDeCobro ("Crédito y Débido", 0.0299, 18, "Mercado Pago 18 días" )
-const mpLink35 = new MetodoDeCobro ("Crédito y Débido", 0.0179, 35, "Mercado Pago 35 días" )
-
-const arrayMetodoDeCobro = [];
-
-arrayMetodoDeCobro.push(mpLink0);
-arrayMetodoDeCobro.push(mpLink10);
-arrayMetodoDeCobro.push(mpLink18);
-arrayMetodoDeCobro.push(mpLink35);
 
 
 /*ARRAY DE LAS RESPUESTAS  */
@@ -96,12 +72,7 @@ const metodosDeCobro = [
 function calculo(){
     //importo las variables
     let monto = parseInt(arrayCalculo);
-    let comision = arrayMetodoDeCobro[0].comision;
-    let metodo = arrayMetodoDeCobro[0].nombre;
     let razon = arrayRespuestas[0];
-
-    //let total = (monto + ((monto * comision) * 1.21));
-    //let costoComision = monto * comision;
 
     const arrayCostos = [];
 
@@ -119,46 +90,16 @@ function calculo(){
     arrayHistorial.push(aux);
    
 
-    //creo el historial
-    //const aux = new MetodoHistorial (razon, metodo, costoComision, total)
-    
-    //cargo el historial
-    //arrayHistorial.push(aux)
     
     arrayCalculo.shift(); //esto borra el valor cargado
     arrayRespuestas.shift();
 
-/*     console.log(arrayRespuestas[0])
-    console.log(total)
-    console.log(aux)
-    console.log(arrayHistorial) */
 
     localStorage.setItem("Respuestas", JSON.stringify(arrayHistorial));
 
 
 }
 
-//const calculo = document.getElementById("calculo")
-
-/* function verHistorial() {
-    const historial = JSON.parse(localStorage.getItem("Respuestas"));
-    let aux = "";
-    historial.forEach (rta => {
-        aux = `
-            <div>
-            <p>Razon: ${rta.razon}</p>
-            <p>Método: ${rta.metodo}</p>
-            <p>Costo de la comisión: ${rta.comision}</p>
-            <p>Total: ${rta.total}</p>
-
-            </div>
-            `
-            + aux;
-    contenedorRespuestas.innerHTML = aux;
-    })
-} */
-
-//    {metodo: "Credito y Debito", comision: 0.0599, dias: 0, nombre: "Mercado Pago 0 días"},
 
 function verHistorial() {
     const historial = JSON.parse(localStorage.getItem("Respuestas"));
