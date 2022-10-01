dd93ccfd80683a0fe970e9ad4eef0c63f3c526a4
Tato López <stefanolopez_1992@hotmail.com>
2022-10-01 20:41:20 -0300

    agregué un selector a todo el proceso

--- comisionador.js
+++ comisionador.js
@@ -5,12 +5,21 @@ const montoAcobrar = document.getElementById("formularioMonto");
 const arrayRespuestas = [];
 const arrayCalculo = [];
 const arrayHistorial = [];
+const arrayMetodosSeleccionados = [];
 
 /* Función de boton */
 montoAcobrar.addEventListener("submit", (e) => {
     e.preventDefault();
+    //Importamos datos a calcular
     ingresarDatos();
+
+    //importar métodos de cobro seleccionados
+    ingresarMetodoDeCobro();
+
+    //Proceso de calculo
     calculo();
+
+    //Creamos el Historial
     verHistorial();
 })
 
@@ -62,10 +71,10 @@ class ClaseHistorial{
 }
 
 const metodosDeCobro = [
-    {metodo: "Credito y Debito", comision: 0.0599, dias: 0, nombre: "Mercado Pago 0 días"},
-    {metodo: "Credito y Debito", comision: 0.0399, dias: 10, nombre: "Mercado Pago 10 días"},
-    {metodo: "Credito y Debito", comision: 0.0299, dias: 18, nombre: "Mercado Pago 18 días"},
-    {metodo: "Credito y Debito", comision: 0.0176, dias: 35, nombre: "Mercado Pago 35 días"},
+    {metodo: "Credito y Debito", comision: 0.0599, dias: 0, nombre: "Mercado Pago 0 días", codigo: "mpLink0D"},
+    {metodo: "Credito y Debito", comision: 0.0399, dias: 10, nombre: "Mercado Pago 10 días", codigo: "mpLink10D"},
+    {metodo: "Credito y Debito", comision: 0.0299, dias: 18, nombre: "Mercado Pago 18 días", codigo: "mpLink18D"},
+    {metodo: "Credito y Debito", comision: 0.0176, dias: 35, nombre: "Mercado Pago 35 días", codigo: "mpLink35D"},
 ];
 
 
@@ -76,7 +85,10 @@ function calculo(){
 
     const arrayCostos = [];
 
-    for (const {comision: c, nombre: i } of metodosDeCobro){
+    //Método funcionado
+    //for (const {comision: c, nombre: i } of metodosDeCobro){
+    
+    for (const {comision: c, nombre: i } of arrayMetodosSeleccionados){
         
         let costoComision = ((monto * c) * 1.21);
 
@@ -87,11 +99,12 @@ function calculo(){
     }
 
     const aux = new ClaseHistorial (razon, arrayCostos);
-    arrayHistorial.push(aux);
+    arrayHistorial.unshift(aux);
    
 
     
     arrayCalculo.shift(); //esto borra el valor cargado
+    arrayMetodosSeleccionados.shift(); //borramos el valor elegido en el método para que no se repita
     arrayRespuestas.shift();
 
 
@@ -100,16 +113,30 @@ function calculo(){
 
 }
 
+function ingresarMetodoDeCobro() {
+    let MPLink = document.getElementById("MPLink").value;
+    
+    console.log(MPLink); //Resultado MPLink
+
+    //necesito encontrar el valor, sacar el indice y pedir que lo devuelva como un array
+    let indexFormaDePago = metodosDeCobro.find(({codigo}) => codigo === MPLink);
+    arrayMetodosSeleccionados.push(indexFormaDePago); 
+    console.log(arrayMetodosSeleccionados);
+
+
+}
+
 
 function verHistorial() {
     const historial = JSON.parse(localStorage.getItem("Respuestas"));
-    let aux = "";
-    console.log(historial);
 
+    let aux = "";
+    
     historial.forEach (rta => {
+      
         aux += `
         <div>
-        <p>Razon: ${rta.razon}</p>
+        <h4>${rta.razon}</h4>
         </div>
         `
         ;
@@ -117,7 +144,7 @@ function verHistorial() {
 
         for (const {comision: c, metodo: m, total: t } of rta.costos){
             total = t;
-            console.log(t);
+            ;
             
             aux = aux +
             `
@@ -136,3 +163,20 @@ function verHistorial() {
     contenedorRespuestas.innerHTML = aux;
     })
 }
+
+//Revisamos si hay o no algo local y lo tomamos
+
+if(localStorage.getItem("Respuestas")){ //esto da True porque el localStorage es distinto de vacio
+    verHistorial(); //solo tengo que llamar a la otra función
+    const historial = JSON.parse(localStorage.getItem("Respuestas"));
+    console.log(historial)
+    arrayHistorial.push(historial);
+    
+}
+
+const limpiarHistoria = document.getElementById("limpiarHistorial");
+
+limpiarHistoria.addEventListener("click", () => {
+    localStorage.removeItem("Respuestas");
+    contenedorRespuestas.innerHTML = "";
+});
